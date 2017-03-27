b2517427bd66197818435c6a6ee8c2a335061da7
Shawn Axsom <axs221@gmail.com>
2017-03-26 21:20:59 -0400

    Testing function, reading Clojure In Action

--- test.clj
+++ test.clj
@@ -3,3 +3,18 @@
 (defn tokenize [r s] (re-seq r s))
 (def result (tokenize reg phrase))
 (println result)
+
+
+(defn print-all-things [things]
+  (let [total (count things) 
+        first-item (first things)]
+        ; last-item (last things)]
+      
+    (defn last-item [my-list] (last my-list))
+
+    (println "first" first-item "last" (last-item things))
+
+    (dorun (map println things))))
+
+
+(print-all-things ["foo" "bar" "baz"])
