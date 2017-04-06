962ca40a6b34540f7b865a76ed2b91a965495a25
Shawn Axsom <axs221@gmail.com>
2017-04-06 07:52:26 -0400

    rewrite prime number function using threading macros

--- prime.clj
+++ prime.clj
@@ -1,15 +1,30 @@
+; (defn prime? [num]
+;   (let [divisors (range 2 (inc (int (Math/sqrt num))))
+;         remainders (filter (fn [x] (= (rem num x) 0)) divisors)]
+;     (= (count remainders) 0)))
+
+; Rewritten using threading macros
 (defn prime? [num]
-  (let [divisors (range 2 (inc (int (Math/sqrt num))))
-        remainders (filter (fn [x] (= (rem num x) 0)) divisors)]
+  (let [divisors (->> num
+                      (Math/sqrt)
+                      (int)
+                      (inc)
+                      (range 2))
+        remainders (filter (fn [x] (as-> x <>
+                                     (rem num <>)
+                                     (= <> 0)
+                                     )) divisors)]
     (= (count remainders) 0)))
 
-(println (prime? 1) 1)
-(println (prime? 5) 5)
-(println (prime? 8) 8)
-(println (prime? 13) 13)
-(println (prime? 18) 18)
-(println (prime? 21) 21)
-(println (prime? 37) 37)
+(defn print-prime [x] (println (prime? x) x))
+
+(print-prime 1)
+(print-prime 5)
+(print-prime 8)
+(print-prime 13)
+(print-prime 18)
+(print-prime 21)
+(print-prime 37)
 
 (defn prime-pairs [num]
   (let [nums (range 2 (inc num))]
